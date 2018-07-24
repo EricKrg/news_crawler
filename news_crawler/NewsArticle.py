fe0bb9a54732b24d0eee6a534653104fee078b15
EricKrg <eric.krueger@uni-jena.de>
2018-07-25 01:11:32 +0200

    add type to constructor

--- news_crawler/NewsArticle.py
+++ news_crawler/NewsArticle.py
@@ -1,10 +1,10 @@
-
 class NewsArticle(object):
-    def __init__(self, title, content):
+    def __init__(self, title, content, type):
         self.title = title
         self.content = content
+        self.type = type
 
     def show(self):
         print self.title
         print self.content
-
+        print self.type
