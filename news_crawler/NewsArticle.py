1f26394d3e915669a04e1fa72d82080a26b35a03
EricKrg <eric.krueger@uni-jena.de>
2018-08-09 19:04:23 +0200

    new fun to markdown

--- news_crawler/NewsArticle.py
+++ news_crawler/NewsArticle.py
@@ -8,3 +8,7 @@ class NewsArticle(object):
         print(self.title)
         print(self.content)
         print(self.type)
+
+    def to_markdown(self):
+        return '### ' + self.title + "\n" + self.content
+ 
