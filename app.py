99f00f0236bb20712d9517180bf5c2f821f2fb69
EricKrg <eric.krueger@uni-jena.de>
2018-08-10 17:10:29 +0200

    added  update button

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 
 # data from crawler module
 import news_crawler
-fetcher_list = [
-    news_crawler.RtFetcher("https://www.rt.com/news"),
-    news_crawler.NyTimesFetcher("https://www.nytimes.com/"),
-    news_crawler.BBCFetcher("https://www.bbc.com/news")
-]
 
-# this is not here to stay 
-bbc_text = rt_text = ny_text = ''
-for fetcher in fetcher_list:
-    for article in fetcher.fetch():
-        if article.type == 'BBC':
-            bbc_text = bbc_text + "\n" + article.to_markdown()
-        if article.type == 'NyTimes':
-            ny_text = ny_text + "\n" + article.to_markdown()
-        if article.type == 'RT':
-            rt_text = rt_text + "\n" + article.to_markdown()
-    
+articles = news_crawler.FetchList()
 # app call
 app = dash.Dash()
 
@@ -33,15 +18,44 @@ article_style = dict(width = '30%', verticalAlign='top',
     color = colors['text'], display='inline-block')
 
 app.layout = html.Div([
+    html.Button(id='submit_bttn',
+                n_clicks=0,
+                children='Update',
+                style=dict(fontSize='24')),  # add a submmit button
     html.Div([html.H1('NY-Times'),
-        dcc.Markdown(children = ny_text)],style = article_style),
+        dcc.Markdown(id='ny')],style = article_style),
     html.Div([html.H1('Russia Today'),
-        dcc.Markdown(children = rt_text)], style = article_style),
+        dcc.Markdown(id = 'rt')], style = article_style),
     html.Div([html.H1('BBC'),
-        dcc.Markdown(children = bbc_text)], style = article_style)
+        dcc.Markdown(id = 'bbc')], style = article_style)
 ], style = dict(backgroundColor = colors['background']))
 
 # callbacks for interactivity
+@app.callback(Output('ny', 'children'),
+              [Input('submit_bttn', 'n_clicks')])
+def output(n):
+    ny_text = ''
+    for article in articles.article_list(keys =['ny']):
+        ny_text = ny_text + "\n" + article.to_markdown().content
+    return ny_text
+
+@app.callback(Output('rt', 'children'),
+              [Input('submit_bttn', 'n_clicks')])
+def output_rt(n):
+    rt_text = ''
+    for article in articles.article_list(keys =['rt']):
+        rt_text = rt_text + "\n" + article.to_markdown().content
+    return rt_text
+
+@app.callback(Output('bbc', 'children'),
+              [Input('submit_bttn', 'n_clicks')])
+def output_bbc(n):
+    bbc_text = ''
+    for article in articles.article_list(keys =['bbc']):
+        bbc_text = bbc_text + "\n" + article.to_markdown().content
+    return bbc_text
+
+
 
 # run
 if __name__ == '__main__':
