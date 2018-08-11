71c5d7f89bc46706b0288390b11c3e6fa4cac451
EricKrg <eric.krueger@uni-jena.de>
2018-08-11 19:43:53 +0200

    added function to auto. create html for each provider

--- app.py
+++ app.py
@@ -6,28 +6,42 @@ from dash.dependencies import Input, Output, State
 
 # data from crawler module
 import news_crawler
-
+# call to create a list with all available fetchers
 articles = news_crawler.FetchList()
 # app call
 app = dash.Dash()
 
 # app layout
-# start off with three divs
+# 
 colors = {'background':'#111111','text':'#ffffff'} # nice way of org. colors
 article_style = dict(width = '30%', verticalAlign='top',
     color = colors['text'], display='inline-block')
 
+def create_provider_layout(providers):
+    html_out = {}
+    for news_provider in providers:
+        html_out[news_provider] = html.Div([html.H1(providers[news_provider]),
+        html.Details([html.Summary('See all:'),
+            dcc.Markdown(id=news_provider)], open = True )],
+        style = article_style)
+    return html_out
+
+# layout
+# this maybe confusing but useful, if more news providers are added to the news feed
+# the individual layout off each provider is created by the 'create_provider_layout()' 
+# this returns a dictonary of created html, this way it could be added to the app layout 
+# by passing the key like: provider_layout['ny']
+
+providers = {'ny':'NY-Times', 'rt':'Russia Today', 'bbc':'BBC'}
+provider_layout = create_provider_layout(providers)
 app.layout = html.Div([
     html.Button(id='submit_bttn',
                 n_clicks=0,
                 children='Update',
                 style=dict(fontSize='24')),  # add a submmit button
-    html.Div([html.H1('NY-Times'),
-        dcc.Markdown(id='ny')],style = article_style),
-    html.Div([html.H1('Russia Today'),
-        dcc.Markdown(id = 'rt')], style = article_style),
-    html.Div([html.H1('BBC'),
-        dcc.Markdown(id = 'bbc')], style = article_style)
+    provider_layout['ny'],
+    provider_layout['rt'],
+    provider_layout['bbc']
 ], style = dict(backgroundColor = colors['background']))
 
 # callbacks for interactivity
@@ -35,7 +49,7 @@ app.layout = html.Div([
               [Input('submit_bttn', 'n_clicks')])
 def output(n):
     ny_text = ''
-    for article in articles.article_list(keys =['ny']):
+    for article in articles.fetch_all(keys =['ny']):
         ny_text = ny_text + "\n" + article.to_markdown().content
     return ny_text
 
@@ -43,7 +57,7 @@ def output(n):
               [Input('submit_bttn', 'n_clicks')])
 def output_rt(n):
     rt_text = ''
-    for article in articles.article_list(keys =['rt']):
+    for article in articles.fetch_all(keys =['rt']):
         rt_text = rt_text + "\n" + article.to_markdown().content
     return rt_text
 
@@ -51,7 +65,7 @@ def output_rt(n):
               [Input('submit_bttn', 'n_clicks')])
 def output_bbc(n):
     bbc_text = ''
-    for article in articles.article_list(keys =['bbc']):
+    for article in articles.fetch_all(keys =['bbc']):
         bbc_text = bbc_text + "\n" + article.to_markdown().content
     return bbc_text
 
