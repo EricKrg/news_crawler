9c6d7531734c513f2d648ed4adc565fe8a93fc1e
jong42 <jonasguetter@gmx.de>
2018-09-22 15:35:53 +0200

    changed folder structure and added code

---
+++ sentiment_analysis/sentiment_analysis.py
@@ -0,0 +1,11 @@
+##################################################################################
+# Name: sentiment analysis.py
+# Author: Jonas Gütter
+# Description: Perform sentiment analysis on data fetched by the News Crawler using 
+# the bag-of-words model and the SentiWordNet lexicon 
+##################################################################################
+
+
+# downloaded September 2018 from http://sentiwordnet.isti.cnr.it/
+
+### Prepare lexicon
