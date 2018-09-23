586a542e96b42d8e507dbd4045e68d77b3da568a
xi86xev <jonas.aaron.guetter@uni-jena.de>
2018-09-23 10:54:46 +0200

    added visualisation of results

--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -8,7 +8,11 @@
 import pandas as pd
 import numpy as np
 import re
-
+import scipy.stats as ss
+import matplotlib.pyplot as plt
+from collections import Counter
+import plotly.plotly as py
+import plotly.tools as tls
 
 #%%Prepare lexicon
 # downloaded September 2018 from http://sentiwordnet.isti.cnr.it/
@@ -20,7 +24,7 @@ lexicon.SynsetTerms = [re.sub('#[abcdefghijklmnopqrstuvwxyz1234567890_ ]*','',wo
                 
 #%%  Get news articles
 
-articles = pd.read_csv('news_articles.csv',sep=',', header=None,names=['ID','source','content'], index_col = 0, skiprows = 1)                              
+articles = pd.read_csv('example_articles.csv',sep='\t', header=None,names=['ID','source','title','content'], index_col = 0, skiprows = 1)                              
                               
 #%% Create corpus
 
@@ -53,8 +57,54 @@ for column,article in enumerate(articles.content):
         
 sentiment_score = np.inner(np.transpose(articles_encoded),corpus.PosScore)   -  np.inner(np.transpose(articles_encoded),corpus.NegScore)
 
-#%%
+#%% Sort data and calculate rank
+articles['sentiment_score'] = sentiment_score
+articles['sentiment_rank'] = ss.rankdata(sentiment_score * (-1))
+articles = articles.sort_values(by = 'sentiment_rank')
 
-# TODO: Titel dazunehmen
-# Rang bestimmen
-# Balkendiagramm erstellen    
+#%% Visualize data 
+
+articles_aj = articles[articles.source == 'AJ']
+articles_bbc = articles[articles.source == 'BBC']
+articles_rt = articles[articles.source == 'RT']
+
+
+occurrences = Counter(articles.sentiment_score)
+occurrences_aj_ref = Counter(articles_aj.sentiment_score)
+occurrences_bbc_ref = Counter(articles_bbc.sentiment_score)
+occurrences_rt_ref = Counter(articles_rt.sentiment_score)
+
+occurrences_aj =  {}
+occurrences_bbc = {}
+occurrences_rt = {}
+
+# Create dicts for each source. It's necessary that each dict has the same keys in the same order
+for element in occurrences:
+    if(element not in occurrences_aj_ref):
+        occurrences_aj[element] = 0
+    else:
+        occurrences_aj[element] = occurrences_aj_ref[element]
+    if(element not in occurrences_bbc_ref):
+        occurrences_bbc[element] = 0
+    else:
+        occurrences_bbc[element] = occurrences_bbc_ref[element]
+    if(element not in occurrences_rt_ref):
+        occurrences_rt[element] = 0
+    else:
+        occurrences_rt[element] = occurrences_rt_ref[element]
+
+
+
+width = 0.1
+p1 = plt.bar(occurrences_aj.keys(), list(occurrences_aj.values()), width, color= 'blue')
+p2 = plt.bar(occurrences_bbc.keys(), list(occurrences_bbc.values()), width, color="red", bottom = list(occurrences_aj.values()))
+p3 = plt.bar(occurrences_rt.keys(), list(occurrences_rt.values()), width, color="green", bottom = [sum(x) for x in zip(list(occurrences_aj.values()), list(occurrences_bbc.values()))])
+
+plt.ylabel('Frequencies')
+plt.xlabel('Sentiment Scores')
+plt.legend((p1[0], p2[0], p3[0]), ('Al Jazeera', 'BBC', 'Russia Today'))
+
+plt.show()
+#fig = plt.gcf()
+#plotly_fig = tls.mpl_to_plotly(fig)
+#py.iplot(plotly_fig, filename='basic-barchart')    
